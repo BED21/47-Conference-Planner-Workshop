8807c9104b7cd84c1af69bc495f5a9cbf3a1d237
Claes Engelin <claes.engelin@zocom.se>
2022-06-09 09:38:35 +0200

    En studerande kom litet sent ;-)

--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -1,4 +1,21 @@
+using GraphQL.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddDbContext<ApplicationDbContext>(options => 
+//            options.UseSqlite("Data Source=conferences.db"));
+builder.Services.AddDbContext<ApplicationDbContext>(
+        (s, opt) =>
+            opt.UseSqlite($"Data Source=conferences.db")
+            .LogTo(Console.WriteLine, new[]
+            {
+                DbLoggerCategory.Database.Command.Name
+            }));
+
+
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ GraphQL/Query.cs
@@ -0,0 +1,8 @@
+﻿using GraphQL.Data;
+
+namespace GraphQL;
+public class Query
+{
+    public IQueryable<Speaker> GetSpeakers([Service] ApplicationDbContext context) =>
+            context.Speakers;
+}
